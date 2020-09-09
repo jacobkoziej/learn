db0954d3ad1124a4ab828a44db4e0f6209bed201
Jacob Koziej <jacobkoziej@gmail.com>
2020-09-09 10:37:52 -0400

    Add "ex2"

---
+++ code/arduino-avr-programming/ex2/ex2.ino
@@ -0,0 +1,19 @@
+/*
+ * Exercise 2
+ * Macro Definitions & Build Automation
+ */
+
+#define LED LED_BUILTIN
+#define DELAY 1000
+
+void setup()
+{
+	pinMode(LED, OUTPUT);
+}
+
+void loop()
+{
+	// Toggle LED state every DELAY
+	digitalWrite(LED, !digitalRead(LED));
+	delay(DELAY);
+}
